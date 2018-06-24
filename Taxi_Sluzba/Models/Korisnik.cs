025efb5fe365f0c7f5560e488ef4607c2e92d8e9
Dacho <Dacho@DACHO-PC>
2018-06-24 12:13:05 +0200

    Promenjen dizajn Login i Registration formi. Implementirana client side validacija

--- Taxi_Sluzba/Models/Korisnik.cs
+++ Taxi_Sluzba/Models/Korisnik.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using Taxi_Sluzba.Enums;
@@ -14,16 +15,30 @@ namespace Taxi_Sluzba.Models
 
         [Required(ErrorMessage ="Username is required")]
         public string UserName { get; set; }
+
         [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Ime je obavezno")]        
         public string Ime { get; set; }
+
+        [Required(ErrorMessage = "Prezime je obavezno")]
         public string Prezime { get; set; }
+
+        [MinLength(13, ErrorMessage ="JMBG mora imati 13 cifara")]
+        [MaxLength(13, ErrorMessage = "JMBG mora imati 13 cifara")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid JMBG")]
         public string JMBG { get; set; }
-        [DisplayName("Pol:")]
+
         public Pol Pol { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid Number")]
         public string Telefon { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        //uloga ??
+
         public List<Voznja> Voznje { get; set; }
 
         public Uloge Uloga { get; set; }
